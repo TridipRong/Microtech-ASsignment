2fd54fe01192523e5aedd56bd4ec291a4e9a2e35
Tridip Rong <rtridip2@gmail.com>
2023-06-30 21:15:51 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -4,32 +4,37 @@ public class Pattern {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter an integer: ");
         int n = scanner.nextInt();
-        int numSpaces = n - 1; // Initialize the number of spaces in each row
-        int numAlphabets = 1; // Initialize the number of alphabets in each row
-
-        for (int i = 0; i < n; i++) {
-            // Loop for each row
-            
-            // Print the required number of spaces before the alphabets
-            for (int j = 0; j < numSpaces; j++) {
-                System.out.print("  ");
+        if(n<=26){
+            int numSpaces = n - 1; // Initialize the number of spaces in each row
+            int numAlphabets = 1; // Initialize the number of alphabets in each row
+    
+            for (int i = 0; i < n; i++) {
+                // Loop for each row
+                
+                // Print the required number of spaces before the alphabets
+                for (int j = 0; j < numSpaces; j++) {
+                    System.out.print("  ");
+                }
+    
+                // Print the alphabets in increasing order
+                for (int j = 0; j < numAlphabets; j++) {
+                    char alphabet = (char) ('A' + i - j); // Calculate the alphabet to be printed
+                    System.out.print(alphabet + " ");
+                }
+    
+                // Print the alphabets in decreasing order, excluding the middle one
+                for (int j = numAlphabets - 2; j >= 0; j--) {
+                    char alphabet = (char) ('A' + i - j); // Calculate the alphabet to be printed
+                    System.out.print(alphabet + " ");
+                }
+    
+                System.out.println(); // Move to the next line after printing each row
+                numSpaces--; // Decrease the number of spaces for the next row
+                numAlphabets++; // Increase the number of alphabets for the next row
+                }
             }
-
-            // Print the alphabets in increasing order
-            for (int j = 0; j < numAlphabets; j++) {
-                char alphabet = (char) ('A' + i - j); // Calculate the alphabet to be printed
-                System.out.print(alphabet + " ");
-            }
-
-            // Print the alphabets in decreasing order, excluding the middle one
-            for (int j = numAlphabets - 2; j >= 0; j--) {
-                char alphabet = (char) ('A' + i - j); // Calculate the alphabet to be printed
-                System.out.print(alphabet + " ");
-            }
-
-            System.out.println(); // Move to the next line after printing each row
-            numSpaces--; // Decrease the number of spaces for the next row
-            numAlphabets++; // Increase the number of alphabets for the next row
+        else{
+             System.out.println("Enter row number less than  or equal 26");
         }
     }
 }
