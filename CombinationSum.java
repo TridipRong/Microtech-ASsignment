ae321d6db0dbc11dc1bb37d9707d51bd4a92764b
Tridip Rong <rtridip2@gmail.com>
2023-06-30 18:40:59 +0530

    Update CombinationSum.java

--- CombinationSum.java
+++ CombinationSum.java
@@ -4,21 +4,23 @@ import java.util.List;
 
 public class CombinationSum {
     public static List<List<Integer>> combinationSum(int[] arr, int targetSum) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> combination = new ArrayList<>();
-        Arrays.sort(arr);
-        backtrack(result, combination, arr, targetSum, 0);
-        return result;
+        List<List<Integer>> result = new ArrayList<>(); // List to store combinations
+        List<Integer> combination = new ArrayList<>(); // Current combination being built
+        Arrays.sort(arr); // Sort the input array in ascending order
+        backtrack(result, combination, arr, targetSum, 0); // Call the helper function
+        return result; // Return the list of combinations
     }
 
     private static void backtrack(List<List<Integer>> result, List<Integer> combination, int[] arr, int target, int start) {
         if (target == 0) {
+            // If target sum becomes zero, add the combination to the result list
             result.add(new ArrayList<>(combination));
         } else if (target > 0) {
+            // If target sum is still positive, continue building combinations
             for (int i = start; i < arr.length; i++) {
-                combination.add(arr[i]);
-                backtrack(result, combination, arr, target - arr[i], i);
-                combination.remove(combination.size() - 1);
+                combination.add(arr[i]); // Add current element to the combination
+                backtrack(result, combination, arr, target - arr[i], i); // Recursively call with updated target and start index
+                combination.remove(combination.size() - 1); // Remove last element for backtracking
             }
         }
     }
@@ -29,10 +31,10 @@ public class CombinationSum {
         List<List<Integer>> combinations = combinationSum(arr, targetSum);
 
         if (combinations.isEmpty()) {
-            System.out.println("Empty");
+            System.out.println("Empty"); // No combinations found
         } else {
             for (List<Integer> combination : combinations) {
-                System.out.println(combination);
+                System.out.println(combination); // Print each combination
             }
         }
     }
